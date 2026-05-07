566c4246b62c2b78034c68da0964d3dcc7758e42
Fellipe <felipefiorito@gmail.com>
2026-05-07 16:02:49 -0300

    first terraform commit

---
+++ terraform/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+
+    backend "s3" {
+        bucket = "fsl-challenge-tfstate-992382773369-us-east-1-an"
+        key    = "tfstates"
+        region = "us-east-1"
+        use_lockfile = true
+    }
+}
+
+provider "aws" {
+    regiont = var.aws_region
+}
